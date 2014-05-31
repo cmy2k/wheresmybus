d0fd80002e461281ecb36258b85a1fe04ccb6d6b
Jay Hill <jayhill@jayhill.net>
2014-05-31 17:45:53 -0400

    added RouteColorsController and PowerPoint

--- server/AvlBusData/Controllers/RouteColorsController.cs
+++ server/AvlBusData/Controllers/RouteColorsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.Http.Results;
+using Newtonsoft.Json;
 
 namespace AvlBusData.Controllers
 {
@@ -11,7 +12,13 @@ namespace AvlBusData.Controllers
     {
         public JsonResult<Dictionary<string, string>> Get()
         {
-            return Json(Data.Routes.ToDictionary(x => x.Value.Title, x => x.Value.ColorHex), Data.JsonSettings);
+            return Json(Data.Routes.ToDictionary(
+                x => x.Value.Title, 
+                x => "#" + x.Value.ColorHex),
+                new JsonSerializerSettings
+                {
+                    Formatting = Formatting.Indented
+                });
         }
     }
 }
